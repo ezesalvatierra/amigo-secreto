ba96f827984117c95569931beafb15219d99faff
ezesalvatierra <ezequielsalvatierra.r@gmail.com>
2025-02-24 11:01:54 -0300

    Inicializando array para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Creando el array para almacenar nombres
+let amigos = [];

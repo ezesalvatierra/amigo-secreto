51310d83708bfcc1467527ae1b1192fafab33f2c
ezesalvatierra <ezequielsalvatierra.r@gmail.com>
2025-02-24 11:27:44 -0300

    Implementando la funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // Creando el array para almacenar nombres
 let amigos = [];
+
+// Funcion para agregar amigos a la lista
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == ''){
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = ''
+    }
+    console.log(amigo);
+    console.log(amigos);
+}
